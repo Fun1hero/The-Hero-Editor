c7877bdd11c5248693cc42f9e727286e088e6d56
Fun1hero <dima220v@gmail.com>
2018-10-07 15:37:24 +0300

    Table of heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'D.Va' },
+    { id: 12, name: 'Roadhog' },
+    { id: 13, name: 'Junkrat' },
+    { id: 14, name: 'Tracer' },
+    { id: 15, name: 'Lucio' },
+    { id: 16, name: 'McCree' },
+    { id: 17, name: 'Reinhardt' },
+    { id: 18, name: 'Symmetra' },
+    { id: 19, name: 'Brigitte' },
+    { id: 20, name: 'Ana' }
+]
